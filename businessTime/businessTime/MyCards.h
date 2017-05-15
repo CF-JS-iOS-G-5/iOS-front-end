f6c6acfd707f9dfd34f20bb22a6bb5a5b3c8c161
EVE DENISON <evedenison@gmail.com>
2017-05-15 13:42:55 -0700

    added cardCreator, myCard, and Meetup object files

---
+++ businessTime/businessTime/MyCards.h
@@ -0,0 +1,13 @@
+//
+//  MyCards.h
+//  businessTime
+//
+//  Created by Eve Denison on 5/15/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyCards : NSObject
+
+@end
