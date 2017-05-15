7eb54fa27347806ff735f959926ec4fbf26e4267
EVE DENISON <evedenison@gmail.com>
2017-05-15 13:56:19 -0700

    create view controller files for all objects

---
+++ businessTime/businessTime/CardCreatorViewController.m
@@ -0,0 +1,37 @@
+//
+//  CardCreatorViewController.m
+//  businessTime
+//
+//  Created by Eve Denison on 5/15/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import "CardCreatorViewController.h"
+
+@interface CardCreatorViewController ()
+
+@end
+
+@implementation CardCreatorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ businessTime/businessTime/MeetupViewController.m
@@ -0,0 +1,37 @@
+//
+//  MeetupViewController.m
+//  businessTime
+//
+//  Created by Eve Denison on 5/15/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import "MeetupViewController.h"
+
+@interface MeetupViewController ()
+
+@end
+
+@implementation MeetupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
