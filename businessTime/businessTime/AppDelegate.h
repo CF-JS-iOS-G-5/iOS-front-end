3027c1ecb5780bd1f0497b576ccd13373a4babf3
Alex Cahn <alexcahn@gmail.com>
2017-05-15 11:45:24 -0700

    created user object file

---
+++ businessTime/businessTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  businessTime
+//
+//  Created by A Cahn on 5/15/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ businessTime/businessTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  businessTime
+//
+//  Created by A Cahn on 5/15/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
