762643ddcc722cb30fc99e39c3dcafb65613370b
EVE DENISON <evedenison@gmail.com>
2017-05-15 17:07:22 -0700

    fixed AFNetworking Framework issue and added AFOAuth2Manager framework, began get token request

--- businessTime/businessTime/User.m
+++ businessTime/businessTime/User.m
@@ -7,14 +7,7 @@
 //
 
 #import "User.h"
-#import "AFURLRequestSerialization.h"
-
 
 @implementation User
-
-NSURL *baseURL = [NSURL URLWithString:@"https://secure.meetup.com/oauth2/authorize"];
-
-AFOAuth2Manager *OAuth2Manager = []
-
 
 @end
