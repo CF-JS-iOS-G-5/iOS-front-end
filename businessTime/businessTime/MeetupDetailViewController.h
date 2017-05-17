0e915d92f2142ba8c5bfd8ef2751fdaed365424d
EVE DENISON <evedenison@gmail.com>
2017-05-16 18:17:00 -0700

    added MeetupDetailViewController with tableView, MyCardsViewController with collectionView.

---
+++ businessTime/businessTime/MeetupDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MeetupDetailViewController.h
+//  businessTime
+//
+//  Created by Eve Denison on 5/16/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeetupDetailViewController : UIViewController
+
+@end
